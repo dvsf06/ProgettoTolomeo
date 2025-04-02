7a6d5daec674b2a392e333d1c01b95055ead8bd2
Davide Sforza <davidesforza06@gmail.com>
2025-04-02 10:48:21 +0200

    modifica favico

--- src/dettaglioPlaylist.php
+++ src/dettaglioPlaylist.php
@@ -14,6 +14,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Playlist</title>
+    <link rel="icon" type="image/x-icon" href="assets/images/favicon.ico">
 
     <link rel="stylesheet" href="assets/playerStyle.css">
     <link rel="stylesheet" href="assets/styleSearch.css">
