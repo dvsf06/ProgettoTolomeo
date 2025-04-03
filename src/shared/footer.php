4c2511d4026fa6a62f4bf2ba9d2c604799a7c524
Davide Sforza <davidesforza06@gmail.com>
2025-04-03 11:23:53 +0200

    player corrections

--- src/shared/footer.php
+++ src/shared/footer.php
@@ -4,7 +4,7 @@
         <!-- Track Info -->
         <div class="track-info">
             <img src="" alt="Album Cover" class="track-image" id="coverContainer">
-            <div>
+            <div class="track-info-text">
                 <div class="marquee" id=>
                     <span class="h5 mb-1" id="trackNameContainer"></span>
                 </div>
@@ -12,35 +12,37 @@
             </div>
         </div>
 
-        <!-- Progress Bar -->
-        <input type="range" class="progress-bar" id="seek-slider">
+        <div class="controls-container">
+            <!-- Progress Bar -->
+            <input type="range" class="progress-bar" id="seek-slider">
 
-        <!-- Time Info -->
-        <div class="d-flex justify-content-between time-info">
-            <span id="currentTime">0:00</span>
-            <span id="durationIndicator">4:30</span>
-        </div>
+            <!-- Time Info -->
+            <div class="d-flex justify-content-between time-info">
+                <span id="currentTime">0:00</span>
+                <span id="durationIndicator">4:30</span>
+            </div>
 
-        <!-- Controls -->
-        <div class="controls">
-            <button class="control-button">
-                <i class="fas fa-random fa-lg"></i>
-            </button>
-            <button class="control-button" onclick="buttonPrev()">
-                <i class="fas fa-step-backward fa-md"></i>
-            </button>
-            <button class="play-button" id="play-button">
-                <i class="fas fa-play fa-md"></i>
-            </button>
-            <button class="control-button" onclick="buttonNext()">
-                <i class="fas fa-step-forward fa-md"></i>
-            </button>
-            <div class="volume-control">
+            <!-- Controls -->
+            <div class="controls">
                 <button class="control-button">
-                    <i class="fas fa-volume-up"></i>
+                    <i class="fas fa-random fa-lg"></i>
                 </button>
-                <div class="volume-slider">
-                    <div class="volume-level"></div>
+                <button class="control-button" onclick="buttonPrev()">
+                    <i class="fas fa-step-backward fa-md"></i>
+                </button>
+                <button class="play-button" id="play-button">
+                    <i class="fas fa-play fa-md"></i>
+                </button>
+                <button class="control-button" onclick="buttonNext()">
+                    <i class="fas fa-step-forward fa-md"></i>
+                </button>
+                <div class="volume-control">
+                    <button class="control-button">
+                        <i class="fas fa-volume-up"></i>
+                    </button>
+                    <div class="volume-slider">
+                        <div class="volume-level"></div>
+                    </div>
                 </div>
             </div>
         </div>
